7819092cf3661bc8ff88c8d99b5a945272f9b542
ThuanTha <tauan.silva00@gmail.com>
2019-11-20 21:51:37 -0300

    INICIO E RPG

---
+++ src/IMAGENS/rpg/RPG.java
@@ -0,0 +1,25 @@
+
+package rpg;
+
+import java.net.URL;
+
+/**
+ *
+ * @author franc
+ */
+public class RPG {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Inicio().setVisible(true);
+  
+
+        
+        
+        
+    }
+    
+}
